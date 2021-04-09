4ec2ce2863f45cf540c2306860aec2beff1dcd38
Josh <joshuawilliam.barnes@gmail.com>
2021-04-09 12:16:29 +1000

    created CRUD actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,39 +1,39 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: %i[ show edit update destroy ]
   def create
-    @candidate = Candidate.find(params[:candidate_id])
+    # @candidate = Candidate.find(params[:candidate_id])
     @results = PgSearch.multisearch(:query)
     @job = Job.new(job_params)
-    @job.candidate = @candidate
-    @job.user = current_user
+    # @job.candidate = @candidate
+    # @job.user = current_user
     if @job.save
-      redirect_to candidate_job_path(@candidate, @job)
+      redirect_to @job
     else
-      render new
+      render :new
     end
   end
 
   def index
-    @jobs = Job.where(user_id: current_user.id)
+    @jobs = Job.all
   end
 
   def new
-    @candidate = Candidate.find(params[:candidate_id])
+    # @candidate = Candidate.find(params[:candidate_id])
     @job = Job.new
   end
 
   def show
-    make_job
     @candidate = @job.candidate
   end
 
   def update
-    make_job
+    set_job
     @job.save!
     redirect_to job_path(@job)
   end
 
   def destroy
-    make_job
+    set_job
     @job.destroy
     redirect_to root_path
   end
@@ -44,8 +44,7 @@ class JobsController < ApplicationController
     params.require(:job).permit(:candidate_id, :title, :work_type, :company, :workplace_address, :salary, :start_date)
   end
 
-  def make_job
+  def set_job
     @job = Job.find(params[:id])
   end
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-
   root to: 'pages#home'
 
   devise_for :users
-
-  resources :candidates do
-    resources :jobs, only: [:new, :create, :show, :update, :destroy]
-  end
-
+  resources :candidates
+  resources :jobs
 end
